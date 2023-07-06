5ed1d25a248f814c1ff59f7b4e47ab9324c8d2ab
Prerna Dobriyal <prernadobriyal11@gmail.com>
2023-07-07 00:44:56 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution: 
+        # code here 
+    
+    def selectionSort(self, arr,n):
+        #code here
+        arr.sort()
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
