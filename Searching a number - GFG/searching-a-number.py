a108886deab1fdaeeff603b1a22fa166375475ba
Prerna Dobriyal <prernadobriyal11@gmail.com>
2023-07-07 00:40:18 +0530

    Added solution - LeetHub

---
+++ Searching a number - GFG/searching-a-number.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+class Solution:
+
+	
+	def search(self,arr, n, k): 
+    	# code here
+    	if k in arr:
+    	    return arr.index(k)+1
+    	return -1
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n, k=map(int, input().strip().split())
+        arr=list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.search(arr, n, k)
+        print(ans)
+        tc=tc-1
+# } Driver Code Ends
